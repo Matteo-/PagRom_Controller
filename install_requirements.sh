d7f21853a7953498184d48f669720854977fc5ed
Matteo Pagotto <matteopagotto2@gmail.com>
2017-12-15 18:42:17 +0100

    requisiti auto installanti

---
+++ install_requirements.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+#sistema:
+sudo apt-get install -y python3 
+sudo apt-get install -y pip3
+sudo apt-get install -y mysql-server
+sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y python3-mysqldb
+#python3:
+sudo pip3 install pyserial
+sudo pip3 install python-telegram-bot --upgrade
