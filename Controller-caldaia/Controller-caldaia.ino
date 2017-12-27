73bccd9d646400473ad666401b7e0a3945a59d77
Matteo Pagotto <matteopagotto2@gmail.com>
2017-12-27 02:59:27 +0100

    controllo aggiunta segatura v1.0

--- Controller-caldaia/Controller-caldaia.ino
+++ Controller-caldaia/Controller-caldaia.ino
@@ -39,25 +39,44 @@
 #include <AccelStepper.h>
 
 typedef struct {
+
+  bool VERBOSE = false;                          // attiva disattiva il verbose mode
+  
+  //TEMPERATURA
   float tmax = 80.0f,
         tmin = 20.0f,
         t1 = .0f,
         t2 = .0f;                             // temperature (cambiare nome)
-  int segatura_aggiunta = 0;
-  byte livello_segatura = 0;
   bool pompa1, pompa2;
+  
+  //segatura
+  bool stepper_run = false;   //stato dello stepper
+  byte livello_segatura = 0;    //livello segatura silos
+  int segatura_aggiunta = 0;    //segatura aggiunta 
+  bool aggiungi_segatura = false;   //flag di controllo per l'aggiunta di segatura
+  int payload = 120;                 //grammi di segatura da aggiungere
+  int payload_mantenimento = 40;    //grammi di segatura da aggiungere una volta raggiunta la temperatura
+  bool coclea_silos = false;        //stato coclea silos
+  bool coclea_segatura = false;     //stato coclea segatura
 
   void invio() {
       /**
        * legenda:
        * 
        */
-      Serial.print("Temp1=");
+      Serial.print("DATA ");
+      Serial.print("T1=");
       Serial.print(t1);
-      Serial.print(" Temp2=");
+      Serial.print(" T2=");
       Serial.print(t2);
-      Serial.print(" Peso=");
+      Serial.print(" SegAgg=");
       Serial.print(segatura_aggiunta);
+      Serial.print(" ControlSeg=");
+      Serial.print(aggiungi_segatura);
+      Serial.print(" Step=");
+      Serial.print(stepper_run);
+      Serial.print(" CclSilos=");
+      Serial.print(coclea_silos);
       Serial.println();
   }
   
@@ -68,6 +87,10 @@ DATA data;                                    // dati importanti
 
 //TIMER
 Timer t;
+
+//TEMPISTICHE
+int ATTESA_SCARICO_SEGATURA = 2500;
+int ATTESA_RITORNO_BILANCIA = 2000;
 
 //TERMOMETRI
 #define ONE_WIRE_BUS 2                        // il pin 2 viene usato per il bus Onewire
@@ -88,20 +111,25 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
 
 /************************************ funzioni ********************************/
 
-/**
- * formatta e invia i dati al computer centrale
- */
-void invioDati(){
-  data.invio();
+void verbose(String s, bool newline = true) {
+  if(data.VERBOSE) Serial.print(s);
+  if(newline) Serial.println();
 }
 
 /**
  * legge la temperatura dei sensori
  */
 void leggoTemperature() {
-  sensors.requestTemperatures();          // invio il comando di richiesta temperatura
-  data.t1 = sensors.getTempCByIndex(CALDAIA);
-  data.t2 = sensors.getTempCByIndex(BOILER);
+  // molto pesante computazionalmente! da problemi con il motore
+  if(data.stepper_run == false) {
+    verbose("read temp"); //debug
+    sensors.requestTemperatures();          // invio il comando di richiesta temperatura
+    data.t1 = sensors.getTempCByIndex(CALDAIA);
+    data.t2 = sensors.getTempCByIndex(BOILER);
+    //stato();
+  } else {
+    verbose("not read temp");  //debug
+  }
 }
 
 /*
@@ -109,15 +137,39 @@ void leggoTemperature() {
  * 
  * @return peso in grammi
  */
-int leggoPesoSegatura() {
-  return bilancia.get_units(), 0;
+int pesoSegatura() {
+  return bilancia.get_units();
+}
+
+/**
+ * formatta e invia i dati al computer centrale
+ */
+void stato() {
+  data.invio();
 }
 
 /**
  * gestisce il motorino per il ribaltamento della segatura
  */
-void rovescioSegatura() {
-  
+void riposiziona() {
+  verbose("riposiziono bilancia");
+  stepper.moveTo(0);
+  //data.stepper_run = false;          // aggiorno lo stato di azionamento dello stepper
+  //stato();
+}
+void svuotaSegatura() {
+  verbose("svuoto segatura");
+  //data.stepper_run = true;            // aggiorno lo stato di azionamento dello stepper
+  //stato();
+  stepper.moveTo(5000);
+  t.after(ATTESA_RITORNO_BILANCIA, riposiziona);
+}
+
+/**
+ * 
+ */
+void addSegatura() {
+  data.aggiungi_segatura = true;
 }
 
 /**
@@ -126,7 +178,52 @@ void rovescioSegatura() {
  * @return segatura inviata alla caldaia in grammi 
  */
 int controllerSegatura() {
-  
+  if (data.aggiungi_segatura) {
+    if (pesoSegatura() >= data.payload ) {
+        verbose("fermo coclea silos");
+        data.coclea_silos = false;
+        data.segatura_aggiunta = pesoSegatura();
+        data.aggiungi_segatura = false;
+        t.after(ATTESA_SCARICO_SEGATURA, svuotaSegatura);
+        //ricalco il prossimo carico
+        //e imposto il timer che lo richiamerà
+        //TODO impostare payload di mantenimento quando la temperatura è arrivata a regime
+        verbose("calcolo temporizzazione prossimo carico");
+        t.after(10000, addSegatura);
+    } else {
+      if(data.coclea_silos == false) {
+        //avvio coclea chiama segatura (set pin to high)
+        data.coclea_silos = true;
+        verbose("avvio coclea silos");
+      }
+    }
+  } else {
+    data.segatura_aggiunta = 0;
+  }
+
+  //controllo se lo stepper è in fase di movimento
+  if (stepper.currentPosition() == 0) {
+    data.stepper_run = false;
+  } else {
+    data.stepper_run = true;
+  }
+}
+
+/**
+ * debug
+ */
+void test() {
+  int start = millis();
+  data.invio();
+  int fine = millis() - start;
+  Serial.print("Tempo invio(ms): ");
+  Serial.println(fine);  //debug
+
+  start = millis();
+  leggoTemperature();
+  fine = millis() - start;
+  Serial.print("Tempo letture temp(ms): ");
+  Serial.println(fine);  //debug
 }
 /************************************** main **********************************/
 
@@ -136,40 +233,39 @@ void setup(void)
   Serial.println("handshake");    // invio "handshake" per la connessione automatica a python
 
   //TERMOMETRI
+  Serial.print("Inizializzazione Termometri...");
   sensors.begin();
-
-  //BILANCIA
-  bilancia.set_scale(calibration_factor); // imposto il fattore di calibrazione
-  bilancia.tare(20);                      // imposto la tara mettendo a 0 la bilancia
+  Serial.println("OK");
 
   //STEPPER SEGATURA
-  stepper.setSpeed(500);
-  stepper.setMaxSpeed(2000);
-  stepper.setAcceleration(300);
-  stepper.setCurrentPosition(0);
-  stepper.moveTo(10000);    //debug
+  Serial.print("Settaggio motore stepper...");
+  //stepper.setSpeed(5000);
+  stepper.setMaxSpeed(5000);
+  stepper.setAcceleration(100000);
+  //stepper.setCurrentPosition(0);
+  Serial.println("OK");
 
   //TIMER
-  //t.every(1000, invioDati);
-  //t.every(200, leggoTemperature);
+  t.every(1000, stato);
+  t.every(2000, leggoTemperature);
 
   //TESTING
-  //pinMode(11, OUTPUT);
-  //digitalWrite(11, HIGH);
-  //pinMode(12, INPUT);
+  t.after(1000, test);
+  t.after(3000, addSegatura);
+
+  //BILANCIA
+  Serial.print("Calibrazione e tara della bilancia...");
+  bilancia.set_scale(calibration_factor); // imposto il fattore di calibrazione
+  bilancia.tare();                      // imposto la tara mettendo a 0 la bilancia
+  Serial.println("OK");
 }
 
 void loop(void)
 { 
-  //leggoTemperature();
-  //invioDati();
-  
-  // If at the end of travel go to the other end DEBUG
-  if (stepper.distanceToGo() == 0)
-    stepper.moveTo(-stepper.currentPosition());
-
-  //Serial.println(digitalRead(12));  //debug
-    
+  /**
+ * varie funzioni di aggiornamento
+ */
+  controllerSegatura();
   t.update();
   stepper.run();
 }
